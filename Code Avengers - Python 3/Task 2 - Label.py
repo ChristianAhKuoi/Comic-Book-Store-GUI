b3bcd038c6ed4fde8f2269eb5c49ae270ae4c9ca
ChristianAhKuoi <78832931+ChristianAhKuoi@users.noreply.github.com>
2021-05-04 00:18:59 +1200

    Add files via upload

---
+++ Code Avengers - Python 3/Task 2 - Label.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# -------------------- USER INTERFACE CODE --------------#
+
+# Create a window
+root = Tk()
+root.title("Python GUI")
+
+label1 = Label(root, text = "Yellow")
+label1.pack()
+
+# Run the main window loop
+root.mainloop ()
