94c4b4432e76ec5fa916509891ced320080d32bd
ChristianAhKuoi <78832931+ChristianAhKuoi@users.noreply.github.com>
2021-05-12 15:04:50 +1200

    Add files via upload

--- comic_gui_v1.py
+++ comic_gui_v1.py
@@ -9,6 +9,8 @@ class Comic:
         self.title = title
         self.stock = stock
 
+
+
 ##########   FUNCTION AND SETUP - COMIC WINDOW   ##########
 # Functions...
 
@@ -24,14 +26,18 @@ root = Tk()
 root.title("Comic Book Store")
 
 # Set color of color window background
-root.configure(bg=tertiary_color)
+root.configure(bg="white")
 
 # Comic frame
-comic_frame = Frame(root, width=300, height=300, bg=tertiary_color)
-comic_frame.grid(row=0, column=0, padx=10, pady=5)
+comic_frame = Frame(root, width=300, height=300, bg="white")
+comic_frame.pack(padx=10, pady=5)
+
+# Top frame
+top_frame = Frame(comic_frame, width=300, height=250, bg=tertiary_color)
+top_frame.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
 # Comic label - Heading (row 0, column 1)
-comic_label = Label(comic_frame,
+comic_label = Label(top_frame,
                         text="Comic Book Store",
                         font=("Ariel", "16", "bold"),
                         bg=primary_color,
@@ -39,13 +45,31 @@ comic_label = Label(comic_frame,
                         )
 comic_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-# Left frame (row 1, column 0)
-left_frame = Frame(comic_frame, width=300, height=250, bg=accent_color)
-left_frame.grid(row=1, column=0, padx=10, pady=5)
+# Left frame (row 2, column 0)
+left_frame = Frame(comic_frame, width=300, height=250, bg=tertiary_color)
+left_frame.grid(row=2, column=0, padx=10, pady=5)
 
-# Right frame (row 1, column 1)
-left_frame = Frame(comic_frame, width=300, height=250, bg=accent_color)
-left_frame.grid(row=1, column=1, padx=10, pady=5)
+# Right frame (row 2, column 1)
+right_frame = Frame(comic_frame, width=300, height=250, bg=tertiary_color)
+right_frame.grid(row=2, column=1, padx=10, pady=5)
+
+# Sell label - (row 0, column 0)
+sell_label = Label(right_frame,
+                        text="Sell",
+                        font=("Ariel", "16", "bold"),
+                        bg=secondary_color,
+                        padx=150, pady=5
+                        )
+sell_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+# Sell combobox - (row 0, column 1)
+options = ['Vanilla', 'Strawberry', 'Chocolate']
+chosen_comic = StringVar()
+chosen_comic.set(options[0])
+
+comic_combobox = ttk.Combobox(right_frame, textvariable=chosen_comic, state="readonly")
+comic_combobox['values'] = options
+comic_combobox.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
 # Run the main window loop
 root.mainloop()
