5e45bcb350ca8fec4da269afcc7a4036db5fd230
ChristianAhKuoi <78832931+ChristianAhKuoi@users.noreply.github.com>
2021-05-14 11:03:24 +1200

    Add files via upload

--- comic_gui_v2.py
+++ comic_gui_v2.py
@@ -3,14 +3,35 @@ from tkinter import *
 from tkinter import ttk
 
 ##########   CLASS CODE   ##########
-# Class code here...
+class Comic:
+    """This code will store the details of each comic (Super Dude, Lizard Man, Water Woman)"""
+    def __init__(self, name, stock):
+        self.name = name
+        self.stock = stock
+        comic_list.append(self)
 
 ##########   FUNCTION AND SETUP - COMIC WINDOW   ##########
+# Functions...
+def create_name_list():
+  name_list = []
+  for comic in comic_list:
+    name_list.append(comic.name)
+  return name_list
+
 # Formatting Variables...
 primary_color = "#38c8f5" #sky blue
 secondary_color = "#e31679" #deep pink
 tertiary_color = "#242424" #light black
 accent_color = "#a6e5d0" #pale blue
+
+# Setup Lists
+comic_list = []
+
+# Creating instances of comic class
+super_dude = Comic("Super Dude", 8)
+lizard_man = Comic("Lizard Man", 12)
+water_woman = Comic("Water Woman", 3)
+comic_names = create_name_list()
 
 ##########   COMIC GUI CODE   ##########
 # Create a window with a title
@@ -67,6 +88,42 @@ mode_label = Label(right_frame,
                         padx=150, pady=5
                         )
 mode_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+# Mode label - (row 1, column 0)
+mode_label = Label(right_frame,
+                        text="Mode:",
+                        font=("Ariel", "12", "bold"),
+                        bg=accent_color,
+                        padx=10, pady=5,
+                        )
+mode_label.grid(row=1, column=0, padx=10, pady=5)
+
+# Radiobutton LabelFrame - (row 1, column 1)
+mode_label_frame = LabelFrame(right_frame, bg=accent_color, bd=0)
+mode_label_frame.grid(row=1, column=1, columnspan=1, padx=10, pady=5)
+
+# Mode(Sell/Restock) Radiobutton
+# Sell Radiobutton (row 0, column 0)
+sell_radio = ttk.Radiobutton(mode_label_frame, text="Sell", value="sell"
+                              )
+sell_radio.grid(row=0, column=0, padx=10, pady=5)
+
+# Restock Radiobutton (row 0, column 1)
+sell_radio = ttk.Radiobutton(mode_label_frame, text="Restock", value="restock"
+                              )
+sell_radio.grid(row=0, column=1, padx=10, pady=5)
+
+# Comic Combobox - (row 2, column 0)
+# Setup variable and option list for the account Combobox
+chosen_comic = StringVar()
+chosen_comic.set(comic_names[0])
+
+# Create a Combobox to select the comic
+comic_combobox = ttk.Combobox(right_frame, textvariable=chosen_comic, state="readonly",
+                              font=("Ariel", "12"),
+                              width=20)
+comic_combobox['values'] = comic_names
+comic_combobox.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
 # Run the main window loop
 root.mainloop()
